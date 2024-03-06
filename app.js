d09bcdbc358ffc5e81526a8f9216379e1d771d4a
Rosario <rosario.egui@gmail.com>
2024-03-05 21:26:42 -0300

    Agregamos las reglas de encriptacion y las aplicamos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+// Creamos funcion para generar la introduccion a la encriptacion
+function processText() {
+    const inputText = document.getElementById('inputText').value;
+    const option = document.getElementById('option').value;
+    let result = '';
+
+    if (option === 'encrypt') {
+        result = encryptText(inputText);
+    } else if (option === 'decrypt') {
+        result = decryptText(inputText);
+    }
+
+    const outputText = document.getElementById('outputText');
+    outputText.value = result;
+
+    // Habilita el botón de copiar cuando se genera un resultado
+    const copyButton = document.getElementById('copyButton');
+    copyButton.disabled = false;
+}
 
